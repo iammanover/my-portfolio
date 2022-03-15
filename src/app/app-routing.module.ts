328dd868e50678461086017b7c2834f4cc3f61b8
ManoverKamaal <manover786kamaal@gmail.com>
2022-03-15 18:53:25 +0530

    my first portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { SoonComponent } from './soon/soon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"logo",
+    component:BannerComponent
+  },
+ {
+  path:"About",
+  component:AboutComponent
+ },
+ {
+  path:"Services",
+  component:ServicesComponent
+ },
+ {
+  path:"Home",
+  component:BannerComponent
+ },
+ 
+ {
+  path:"Portfolio",
+  component:SoonComponent
+ },
+ {
+  path:"Blog",
+  component:SoonComponent
+ },
+ {
+  path:"Contact",
+  component:ContactComponent
+ },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { SoonComponent } from './soon/soon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    SoonComponent
   ],
   imports: [
     BrowserModule,
